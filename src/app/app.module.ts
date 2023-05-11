697b4e82f21b8ebc42930cdaa1cd79d2cc42cf2f
Fulano de Tal <deborabbayer@hotmail.com>
2023-05-11 09:44:58 -0300

    build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { App1RoutingModule } from './app1-routing.module';
+import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule} from "@angular/material/snack-bar";
+import { CarrinhoModule } from './carrinho/carrinho.module'
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { ContatoModule } from './contato/contato.module';
+import { BarraPesquisaComponent } from './barra-pesquisa/barra-pesquisa.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NaoEncontradaComponent,
+    BarraPesquisaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    App1RoutingModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    CarrinhoModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
